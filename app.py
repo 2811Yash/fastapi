1f9255063f8ce26b80b1b85f5f057451ce7be0d2
2811Yash <118837106+2811Yash@users.noreply.github.com>
2025-01-27 11:12:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langserve import add_routes
+import uvicorn
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ['GOOGLE_API_KEY']=os.getenv("GOOGLE_API_KEY")
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+app=FastAPI(
+    title="Langchain Server",
+    version="1.0",
+    decsription="A simple API Server"
+
+)
+prompt1=ChatPromptTemplate.from_template("explain {topic} technically answer the questions which are from a technical background only and answer in 100 words only ")
+
+
+add_routes(
+    app,
+    prompt1|llm,
+    path="/essay/invoke"
+)
+
