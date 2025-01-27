28442c0bccfe9ed8494d7c3528fb7836f9c381c2
2811Yash <118837106+2811Yash@users.noreply.github.com>
2025-01-27 16:13:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,19 +12,21 @@ os.environ['GOOGLE_API_KEY']=os.getenv("GOOGLE_API_KEY")
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
 
 # Allow CORS for frontend requests
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Change to your frontend URL in production
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 app=FastAPI(
     title="Langchain Server",
     version="1.0",
     decsription="A simple API Server"
 
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change to your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 prompt1=ChatPromptTemplate.from_template("explain {topic} technically answer the questions which are from a technical background only and answer in 100 words only ")
 
